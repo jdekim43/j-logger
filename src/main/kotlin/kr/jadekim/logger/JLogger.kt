1fba7666b2eecaf385f650fca8c093035e0d8f7b
김진용 <jdekim43@coinone.com>
2019-12-09 14:16:32 +0900

    JLogger lazy function 추가

--- src/main/kotlin/kr/jadekim/logger/JLogger.kt
+++ src/main/kotlin/kr/jadekim/logger/JLogger.kt
@@ -33,24 +33,55 @@ class JLogger(
         }
     }
 
+    fun log(level: Level, body: LogExtra.() -> String) {
+        if (!level.isPrintable(this.level)) {
+            return
+        }
+
+        val extra = LogExtra()
+        val message = body(extra)
+
+        log(level, message, extra.throwable, extra.extra)
+    }
+
     fun error(message: String, throwable: Throwable? = null, extra: Map<String, Any?> = emptyMap()) {
         log(Level.ERROR, message, throwable, extra)
+    }
+
+    fun error(body: LogExtra.() -> String) {
+        log(Level.ERROR, body)
     }
 
     fun warning(message: String, throwable: Throwable? = null, extra: Map<String, Any?> = emptyMap()) {
         log(Level.WARNING, message, throwable, extra)
     }
 
+    fun warning(body: LogExtra.() -> String) {
+        log(Level.WARNING, body)
+    }
+
     fun info(message: String, throwable: Throwable? = null, extra: Map<String, Any?> = emptyMap()) {
         log(Level.INFO, message, throwable, extra)
+    }
+
+    fun info(body: LogExtra.() -> String) {
+        log(Level.INFO, body)
     }
 
     fun debug(message: String, throwable: Throwable? = null, extra: Map<String, Any?> = emptyMap()) {
         log(Level.DEBUG, message, throwable, extra)
     }
 
+    fun debug(body: LogExtra.() -> String) {
+        log(Level.DEBUG, body)
+    }
+
     fun trace(message: String, throwable: Throwable? = null, extra: Map<String, Any?> = emptyMap()) {
         log(Level.TRACE, message, throwable, extra)
+    }
+
+    fun trace(body: LogExtra.() -> String) {
+        log(Level.TRACE, body)
     }
 
     suspend fun sLog(
@@ -72,23 +103,59 @@ class JLogger(
         }
     }
 
+    suspend fun sLog(level: Level, body: LogExtra.() -> String) {
+        if (!level.isPrintable(this.level)) {
+            return
+        }
+
+        val extra = LogExtra()
+        val message = body(extra)
+
+        sLog(level, message, extra.throwable, extra.extra)
+    }
+
     suspend fun sError(message: String, throwable: Throwable? = null, extra: Map<String, Any?> = emptyMap()) {
         sLog(Level.ERROR, message, throwable, extra)
+    }
+
+    suspend fun sError(body: LogExtra.() -> String) {
+        sLog(Level.ERROR, body)
     }
 
     suspend fun sWarning(message: String, throwable: Throwable? = null, extra: Map<String, Any?> = emptyMap()) {
         sLog(Level.WARNING, message, throwable, extra)
     }
 
+    suspend fun sWarning(body: LogExtra.() -> String) {
+        sLog(Level.WARNING, body)
+    }
+
     suspend fun sInfo(message: String, throwable: Throwable? = null, extra: Map<String, Any?> = emptyMap()) {
         sLog(Level.INFO, message, throwable, extra)
+    }
+
+    suspend fun sInfo(body: LogExtra.() -> String) {
+        sLog(Level.INFO, body)
     }
 
     suspend fun sDebug(message: String, throwable: Throwable? = null, extra: Map<String, Any?> = emptyMap()) {
         sLog(Level.DEBUG, message, throwable, extra)
     }
 
+    suspend fun sDebug(body: LogExtra.() -> String) {
+        sLog(Level.DEBUG, body)
+    }
+
     suspend fun sTrace(message: String, throwable: Throwable? = null, extra: Map<String, Any?> = emptyMap()) {
         sLog(Level.TRACE, message, throwable, extra)
     }
+
+    suspend fun sTrace(body: LogExtra.() -> String) {
+        sLog(Level.TRACE, body)
+    }
+}
+
+class LogExtra {
+    var throwable: Throwable? = null
+    var extra: Map<String, Any?> = emptyMap()
 }
