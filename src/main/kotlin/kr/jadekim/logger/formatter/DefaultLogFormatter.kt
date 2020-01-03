7ba9ebf3749720fbbba52c0b647724560a94b875
김진용 <jdekim43@coinone.com>
2020-01-03 17:13:09 +0900

    DefaultLogFormatter 에서 extra 정보를 출력 가능하도록 수정

--- src/main/kotlin/kr/jadekim/logger/formatter/DefaultLogFormatter.kt
+++ src/main/kotlin/kr/jadekim/logger/formatter/DefaultLogFormatter.kt
@@ -5,17 +5,23 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-class DefaultLogFormatter : LogFormatter {
+class DefaultLogFormatter(
+    var printExtra: Boolean = true
+) : LogFormatter {
 
     var dateFormatter: DateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S")
 
     override fun format(log: Log): String {
-        return StringBuilder().apply {
+        val builder = StringBuilder().apply {
             append("%-23s".format(dateFormatter.format(Date(log.timestamp))))
             append(" %-5s".format(log.level.name))
             append(" %-32s".format(log.loggerName))
             append(" [ %-20s ]".format(log.thread))
-            append(" : ${log.message}\n")
-        }.toString()
+            append(" : ${log.message}(")
+        }
+
+        log.extra.map { "${it.key}=${it.value}" }.joinTo(builder, ", ")
+
+        return builder.append(")\n").toString()
     }
 }
