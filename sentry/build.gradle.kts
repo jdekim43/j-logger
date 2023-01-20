03c6f6b09ee61d453b2ce2ad55362b897f2d7aa1
Jade Kim <jade@breezelabs.io>
2023-01-20 12:42:43 +0900

    Add CompositeException

---
+++ sentry/build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    implementation(project(":"))
+
+    implementation("io.sentry:sentry:6.12.1")
+    implementation("io.sentry:sentry-kotlin-extensions:6.12.1")
+}
+
+tasks.withType<KotlinCompile> {
+    val jvmTarget: String by rootProject
+
+    kotlinOptions.jvmTarget = jvmTarget
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications.create<MavenPublication>("lib") {
+        groupId = project.group.toString()
+        artifactId = project.name
+        version = project.version.toString()
+        from(components["java"])
+
+        pom {
+            name.set(project.name)
+            description.set("Logging Library for Kotlin")
+            url.set("https://github.com/jdekim43/j-logger")
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+            }
+            developers {
+                developer {
+                    id.set("jdekim43")
+                    name.set("Jade Kim")
+                }
+            }
+            scm {
+                connection.set("scm:git:git://github.com/jdekim43/j-logger.git")
+                developerConnection.set("scm:git:git://github.com/jdekim43/j-logger.git")
+                url.set("https://github.com/jdekim43/j-logger")
+            }
+        }
+    }
+}
